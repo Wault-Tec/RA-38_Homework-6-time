48548d22db2a3200eca132df6bdedf46ce24e9fb
Tpakmopucm <explay0907@gmail.com>
2022-06-29 20:16:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
 }
 
-export default App;
+function withDateTimePretty(Component) {
+  return function(props) {
+    const now = new Date().getTime();
+    const loadDate = new Date(props.date).getTime();
+    const msOnHour = 60*60*1000;
+    const msOnDay = 24*60*60*1000;
+
+    const getFormatDate = (now, loadDate) => {
+      const dateDiff = now - loadDate;
+      let dateDiffFormat = props.date;
+      if(dateDiff < msOnHour) {
+        dateDiffFormat = Math.floor(dateDiff/(60*1000)) + ' минут назад'
+      } else if(dateDiff >= msOnHour && dateDiff < msOnDay) {
+        dateDiffFormat = Math.floor(dateDiff/(msOnHour)) + ' часов назад'
+      } else if (dateDiff >= msOnDay) {
+        dateDiffFormat = Math.floor(dateDiff/(msOnDay)) + ' дней назад'
+      } 
+      return dateDiffFormat
+    }
+
+    const newProps = {date: getFormatDate(now, loadDate)}
+    return <Component {...newProps}/>
+  }
+}
+
+const DateTimePretty = withDateTimePretty(DateTime)
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            <DateTimePretty date={props.date} />
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video url={item.url} date={item.date} key={item.url}/>);
+}
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-06-29 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-06-29 20:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
